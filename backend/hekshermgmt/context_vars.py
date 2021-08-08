06c6c35b9549d8584867aefb1366d3b42c2f11be
ben avrahami <ben.avrahami@biocatch.com>
2021-08-08 15:39:18 +0300

    type linting?

--- backend/hekshermgmt/context_vars.py
+++ backend/hekshermgmt/context_vars.py
@@ -1,4 +1,5 @@
 from contextvars import ContextVar
+from typing import Optional
 
-user: ContextVar[str] = ContextVar("user", default=None)
+user: ContextVar[Optional[str]] = ContextVar("user", default=None)
 """ Context variable of the user whom initiated the request, used for logging and monitoring """
